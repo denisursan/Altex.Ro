aa659f28f541db2beec9937bfc131cdc1c4e4a72
Denis Ursan <ursandenis96@yahoo.com>
2021-02-21 10:03:09 +0200

    commit

--- src/main/java/utils/MainPage.java
+++ src/main/java/utils/MainPage.java
@@ -1,4 +1,4 @@
-package utils1;
+package utils;
 
 import lombok.Getter;
 import org.openqa.selenium.WebDriver;
--- src/test/java/testImplementation/BaseSetUp.java
+++ src/test/java/testImplementation/BaseSetUp.java
@@ -1,43 +1,13 @@
 package testImplementation;
-<<<<<<< HEAD
-
-import io.cucumber.java.Before;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import utils.LogInPage;
-import utils.Utils;
-import utils1.MainPage;
-=======
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import utils.LogInPage;
 import utils.MainPage;
 import utils.ProductsPage;
->>>>>>> Branch-Denis
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseSetUp {
-<<<<<<< HEAD
-    WebDriver driver;
-    Utils utils;
-    MainPage mainPage;
-    LogInPage logInPage;
-
-
-    BaseSetUp(int wait){
-           System.setProperty("webdriver.chrome.driver", "src/main/resources/seleniumdriver/chromedriver/chromedriver 2");
-           driver = new ChromeDriver();
-           driver.manage().window().maximize();
-           utils = new Utils(driver);
-           driver.manage().timeouts().implicitlyWait(wait, TimeUnit.SECONDS);
-           mainPage = new MainPage(driver);
-           logInPage = new LogInPage(driver);
-
-       }
-   }
-
     MainPage mainPage;
     WebDriver driver;
     LogInPage logInPage;
@@ -53,4 +23,3 @@ public class BaseSetUp {
         productsPage= new ProductsPage(driver);
     }
 }
-
