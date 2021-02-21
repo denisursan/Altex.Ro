372cff1b5c8f20a8bd3b2af4790c1e85f8c62e79
Denis Ursan <ursandenis96@yahoo.com>
2021-02-21 10:35:49 +0200

    search field

--- src/main/java/utils/MainPage.java
+++ src/main/java/utils/MainPage.java
@@ -1,6 +1,7 @@
 package utils;
 
 import lombok.Getter;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
